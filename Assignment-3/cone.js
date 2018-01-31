8018d45db7aeb4ac1230bf41852203a880d3b5e3
Boombott <Briancbotting1@gmail.com>
2018-01-30 18:20:49 -0800

    Update cone.js

--- Assignment-3/cone.js
+++ Assignment-3/cone.js
@@ -1,4 +1,5 @@
 var gl = null;
+var cone = null; 
 
 function init() {
     var canvas = document.getElementById( "webgl-canvas" );
@@ -9,14 +10,16 @@ function init() {
         alert("Unable to setup WebGL");
         return;
     }
-
+    
     gl.clearColor( 0.5, 0.2, 0.5, 0.7 );
-
+    cone = new Cone(gl, n);
+    
     render();
 }
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
+    cone.render();
 }
 
 window.onload = init;
