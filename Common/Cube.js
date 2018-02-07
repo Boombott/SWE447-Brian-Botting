46c9247f53944b4462026f622cd3370d6b28e035
Boombott <Briancbotting1@gmail.com>
2018-02-06 21:53:03 -0800

    Update Cube.js

--- Common/Cube.js
+++ Common/Cube.js
@@ -105,7 +105,13 @@ function Cube( vertexShaderId, fragmentShaderId ) {
     };
     this.indices.count = this.indices.values.length;
 
+    this.colors.buffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, this.colors.buffer );
+    gl.bufferData( gl.ARRAY_BUFFER, this.colors.values, gl.STATIC_DRAW );
+    this.colors.attributeLoc = gl.getAttribLocation( this.program, "vColor" );
+    gl.enableVertexAttribArray( this.colors.attributeLoc );
     
+	
     this.positions.buffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, this.positions.buffer );
     gl.bufferData( gl.ARRAY_BUFFER, this.positions.values, gl.STATIC_DRAW );
