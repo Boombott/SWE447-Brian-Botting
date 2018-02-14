96d6e98703cd6d62eedacb1d7d316fb6131243d8
Boombott <Briancbotting1@gmail.com>
2018-02-13 20:36:32 -0800

    Update button.js

--- Assignment-5/button.js
+++ Assignment-5/button.js
@@ -1,1 +1,13 @@
+var rotationAxis;
+var xAxis = [1, 0, 0];
+var yAxis = [0, 1, 0];
 
+function init() {
+  document.getElementById("xButton").onclick = function() {
+    console.log("Someone pressed this button");
+  }
+}
+
+function render() {
+  ms.rotate(speed * angle, rotationAxis);
+}
